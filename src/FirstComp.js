d41ef688208bb078e73f252c725441a681c07c3d
sourav mehraniya <souravmehraniya0007@gmail.com>
2023-01-18 13:45:54 +0530

    new file

---
+++ src/FirstComp.js
@@ -0,0 +1,10 @@
+
+
+function FirstComp() {
+    return (
+        <div>
+            <h1>This is First Component</h1>
+        </div>
+    );
+}
+export default FirstComp;
---
+++ src/SecondComp.js
@@ -0,0 +1,8 @@
+function SecondComp() {
+    return (
+        <div>
+            <h1>This is Second Component</h1>
+        </div>
+    );
+}
+export default SecondComp;
